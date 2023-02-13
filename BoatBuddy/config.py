15735b42d6170cf6cb8a30e04914c209bdcb7cdf
Joe Zeitouny <joezeitouny@gmail.com>
2023-02-13 15:08:32 +0100

    Version bump to 0.2.14

--- BoatBuddy/config.py
+++ BoatBuddy/config.py
@@ -1,6 +1,6 @@
 # General
 APPLICATION_NAME = 'Boat Buddy'
-APPLICATION_VERSION = '0.2.13'
+APPLICATION_VERSION = '0.2.14'
 LOG_FILENAME = 'BoatBuddy.log'
 LOG_FILE_SIZE = 1024 * 1024  # Log file size 1MB
 LOGGER_NAME = 'BoatBuddy'
